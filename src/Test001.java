a7c0291c309b5bb3f4d42d38b21d579977a0c604
marine1015 <marine1015@163.com>
2018-11-15 16:20:05 +0800

    1234

---
+++ src/Test001.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class Test001 {
+    public static void main(String [] abc){
+        int a = 1;
+
+        float f = Float.valueOf("123.123");
+        System.out.println(a);
+    }
+
+    public int abc(int a){
+        return 123;
+    }
+}
